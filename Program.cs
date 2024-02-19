0c8afc14e36b058b7fdc144b486db3e8f623b0a5
eligami <68642361+eligamii@users.noreply.github.com>
2024-02-19 14:41:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+[DllImport("shell32", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+static extern string SHGetKnownFolderPath(
+       [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags,
+       nint hToken = 0);
+
+string path = SHGetKnownFolderPath(new Guid("F1B32785-6FBA-4FCF-9D55-7B8E7F157091"), 0) + "\\Packages\\TheBrowserCompany.Arc_ttt1ap7aakyb4\\LocalCache\\Local\\firestore\\Arc\\bcny-arc-server\\main";
+
+while (true)
+{
+    try
+    {
+        foreach (string file in Directory.GetFiles(path)) File.Delete(file);
+        var processes = Process.GetProcessesByName("Arc");
+
+        while (processes.Count() == 0)
+        {
+            await Task.Delay(100);
+            processes = processes = Process.GetProcessesByName("Arc");
+        }
+
+        Task.WaitAll(processes.Select(p => p.WaitForExitAsync()).ToArray());
+    }
+    catch { }
+}
